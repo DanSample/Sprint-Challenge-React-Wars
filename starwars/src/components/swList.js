e60b29343dbd704f8f248df98216ef96ec59127a
Dan Sample <danssample@gmail.com>
2019-05-18 10:09:37 -0700

    built out 2 component files swPerson and swList

---
+++ starwars/src/components/swList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SwPerson from './swPerson';
+
+const SwList = props => {
+  return (
+    <ul>
+      <SwPerson />
+    </ul>
+  );
+};
+
+export default SwList;
---
+++ starwars/src/components/swPerson.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SwPerson = () => {
+  return <li />;
+};
+
+export default SwPerson;
